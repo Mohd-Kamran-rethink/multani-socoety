3093ad67c7b86ccbd6ec57f3b643853e4c96bdf4
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-04-04 16:09:47 +0530

    login api

--- app/Http/Controllers/UserApiController/User.php
+++ app/Http/Controllers/UserApiController/User.php
@@ -20,23 +20,23 @@ class User extends Controller
             'phone' => ['required', 'string', 'regex:/^\d{10}$/'],
             'password' => ['required', 'string'],
         ], [
-            'phone.required' => 'The phone field is required.',
-            'phone.string' => 'The phone field must be a string.',
-            'phone.regex' => 'The phone field must be a valid 10-digit phone number.',
-            'password.required' => 'The password field is required.',
-            'password.string' => 'The password field must be a string.',
+            'phone.required' => 'Phone is required.',
+            'phone.string' => 'Phone must be a string.',
+            'phone.regex' => 'Phone must be a valid 10-digit.',
+            'password.required' => 'Password is required.',
+            'password.string' => 'Password must be a string.',
         ]);
 
-        $user = People::where('phone', $validatedData['phone'])->first();
+        $user = People::where('app_registered', 'Yes')->where('phone', $validatedData['phone'])->first();
 
         if (!$user) {
             return response()->json([
-                'phone' => ['The provided phone is not registered with us.'],
+                'phone' => ['This phone number is not registered with us.'],
             ], 401);
         } else {
             if (!Hash::check($validatedData['password'], $user->password)) {
                 return response()->json([
-                    'password' => ['The provided phone and password combination is incorrect.'],
+                    'password' => ['Incorrect password.'],
                 ], 401);
             }
         }
