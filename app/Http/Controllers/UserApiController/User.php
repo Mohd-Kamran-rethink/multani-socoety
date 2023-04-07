bc2ac420aa886f484db544e1eb10f1d5ce05673f
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-04-07 14:35:56 +0530

    sdf

--- app/Http/Controllers/UserApiController/User.php
+++ app/Http/Controllers/UserApiController/User.php
@@ -48,65 +48,65 @@ class User extends Controller
             'data' => ['otp'=>$otp],
         ], 200);
     }
-    // public function getDataRegister()
-    // {
-    //     $laqabs = Laqab::orderBy('laqab', 'ASC')->get();
-    //     $qualifications = Qualification::orderBy('title', 'ASC')->get();
-    //     $states = State::where('country_id', '=', '101')->orderBy('name', 'asc')->get();
-    //      return response()->json([
-    //         'error' => false,
-    //         'message' => 'Data fetched successfully',
-    //         'data' => ['laqabs'=>$laqabs,'qualifications'=>$qualifications,'states'=>$states],
-    //     ], 200);
-    // }
-    // public function getCities(Request $req)
-    // {
-    //     $cities = City::where('state_id', '=', $req->city_id)->orderBy('name', 'asc')->get();
-    //     return response()->json([
-    //         'error' => true,
-    //         'message' => 'The given data was invalid.',
-    //         'data' => ['cities'=>$cities],
-    //     ], 200);
-    // }
-    // public function register(Request $req)
-    // {
-    //     $validator = Validator::make($req->all(), [
-    //         'name' => 'required|string|max:255',
-    //         'phone' => 'required|unique:people,phone',
-    //         'password' => 'required',
-    //     ]);
+    public function getDataRegister()
+    {
+        $laqabs = Laqab::orderBy('laqab', 'ASC')->get();
+        $qualifications = Qualification::orderBy('title', 'ASC')->get();
+        $states = State::where('country_id', '=', '101')->orderBy('name', 'asc')->get();
+         return response()->json([
+            'error' => false,
+            'message' => 'Data fetched successfully',
+            'data' => ['laqabs'=>$laqabs,'qualifications'=>$qualifications,'states'=>$states],
+        ], 200);
+    }
+    public function getCities(Request $req)
+    {
+        $cities = City::where('state_id', '=', $req->city_id)->orderBy('name', 'asc')->get();
+        return response()->json([
+            'error' => true,
+            'message' => 'The given data was invalid.',
+            'data' => ['cities'=>$cities],
+        ], 200);
+    }
+    public function register(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'name' => 'required|string|max:255',
+            'phone' => 'required|unique:people,phone',
+            'password' => 'required',
+        ]);
           
-    //     if ($validator->fails()) {
-    //         $errors = $validator->errors();
-    //         $response = [];
-    //         foreach ($errors->keys() as $key) {
-    //             $response[] = [
-    //                 'field' => $key,
-    //                 'message' => $errors->first($key),
-    //             ];
-    //         }
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            $response = [];
+            foreach ($errors->keys() as $key) {
+                $response[] = [
+                    'field' => $key,
+                    'message' => $errors->first($key),
+                ];
+            }
 
-    //         return response()->json([
-    //             'error' => true,
-    //             'message' => 'The given data was invalid.',
-    //             'data' => $response,
-    //         ], 200);
-    //     } else {
+            return response()->json([
+                'error' => true,
+                'message' => 'The given data was invalid.',
+                'data' => $response,
+            ], 200);
+        } else {
 
-    //         $user = new People();
-    //         $user->name = $req->name;
-    //         $user->phone = $req->phone;
-    //         $user->password = Hash::make($req->password);
-    //         $user->app_registered = "Yes";
-    //         $user->is_profile_shared = "No";
-    //         $result = $user->save();
-    //         if ($result) {
-    //             return response()->json([
-    //                 'error' => false,
-    //                 'message' => 'Successfully registered',
-    //                 'data' => $user,
-    //             ], 200);
-    //         }
-    //     }
-    // }
+            $user = new People();
+            $user->name = $req->name;
+            $user->phone = $req->phone;
+            $user->password = Hash::make($req->password);
+            $user->app_registered = "Yes";
+            $user->is_profile_shared = "No";
+            $result = $user->save();
+            if ($result) {
+                return response()->json([
+                    'error' => false,
+                    'message' => 'Successfully registered',
+                    'data' => $user,
+                ], 200);
+            }
+        }
+    }
 }
